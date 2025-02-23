0b9eeeae5f866d111b6059d15d21d99485923566
LeatherFire <71224249+LeatherFire@users.noreply.github.com>
2025-02-23 15:36:10 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // "font-mono" class'ı artık Noto Sans Mono kullanacak
+        mono: ["'Noto Sans Mono'", "Courier New", "monospace"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
